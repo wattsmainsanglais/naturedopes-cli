b8b196a0c1cabf86be81529fd6d342de848cee6c
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-11-22 14:41:36 +0100

    phase 4 complete, images x2 routes created and cobra commands set

--- cmd/images.go
+++ cmd/images.go
@@ -2,10 +2,10 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/wattsmainsanglais/naturedopes-cli/pkg/api"
 	"github.com/wattsmainsanglais/naturedopes-cli/pkg/config"
+	"strconv"
 )
 
 var imagesCmd = &cobra.Command{
@@ -24,7 +24,7 @@ var listImagesCmd = &cobra.Command{
 
 		resp, err := client.ListImages()
 		if err != nil {
-			fmt.Errorf("could not retrieve images: %w", err)
+			fmt.Printf("could not retrieve images: %v\n", err)
 			return
 		}
 
@@ -36,11 +36,35 @@ var listImagesCmd = &cobra.Command{
 }
 
 var getImageCmd = &cobra.Command{
-	Use:   "get --id <id>",
+	Use:   "get <id>",
 	Short: "Get individual image",
 	Args:  cobra.ExactArgs(1),
 	Run: func(command *cobra.Command, args []string) {
 		id := args[0]
+		integer, err := strconv.Atoi(id)
+		if err != nil {
+			fmt.Printf("Error, invalid ID, please check you've supplied an integer as argument: %v\n", err)
+			return
+		}
+
+		baseUrl, _ := config.Get("api-url")
+		key, _ := config.Get("api-key")
+		client := api.NewClient(baseUrl, key)
+
+		image, err := client.GetImage(integer)
+		if err != nil {
+			fmt.Printf("could not retrieve image data: %v\n", err)
+			return
+		}
+
+		fmt.Printf("id:%d name: %s, gps_long: %f, gps_lat: %f, image_path: %s\n", image.ID, image.SpeciesName, image.GpsLong, image.GpsLat, image.ImagePath)
 
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(imagesCmd)
+	imagesCmd.AddCommand(listImagesCmd)
+	imagesCmd.AddCommand(getImageCmd)
+
 }
